bb01d372dbdf6676f7d19a916d1ee3e09a273695
Set <igogowka@gmail.com>
2015-10-16 19:24:43 +0300

    main.cpp commit

---
+++ repo/main.cpp
@@ -0,0 +1,137 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <iomanip>  
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/core/ref.hpp>
+#include <boost/timer/timer.hpp>
+#include <boost/chrono.hpp>
+#include "Windows.h"
+using namespace std;
+
+struct THR {
+	boost::random::mt19937 gen;
+	int id;
+	LARGE_INTEGER StartingTime, EndingTime, ElapsedNanoseconds;
+	boost::chrono::nanoseconds  elapse;
+	boost::timer::cpu_timer cpu;
+};
+
+class EvenOddFunctor {
+	int even_;
+	int odd_;
+public:
+	EvenOddFunctor() : even_(0), odd_(0) {
+
+	}
+	void operator()(int x) {
+		if (x % 2 == 0) even_ += x;
+		else odd_ += x;
+	}
+	int even_sum() const { return even_; }
+	int odd_sum() const { return odd_; }
+};
+
+int roll_die(boost::random::mt19937 &gen) {
+	boost::random::uniform_int_distribution<> dist(1, 999999);
+	return dist(gen);
+}
+
+void thread_func(THR &str_thr, LARGE_INTEGER &Frequency) {
+	int var = 0, i = 0;
+	static int varvar = 0;
+	str_thr.ElapsedNanoseconds.QuadPart = 0;
+	vector<vector<LONGLONG>> str_vctr;
+	//str_thr.elapse.zero();
+
+	//str_thr.cpu.is_stopped() ? str_thr.cpu.resume() : str_thr.cpu.start();
+	QueryPerformanceCounter(&str_thr.StartingTime);
+	str_thr.gen.seed(static_cast<uint32_t>(str_thr.StartingTime.QuadPart));
+	while (var != 444 && varvar != 444) {
+		QueryPerformanceCounter(&str_thr.StartingTime);
+		//str_thr.cpu.is_stopped() ? str_thr.cpu.resume() : str_thr.cpu.start();
+		//str_thr.gen.seed(str_thr.StartingTime.QuadPart);
+		var = roll_die(str_thr.gen);
+		QueryPerformanceCounter(&str_thr.EndingTime);
+		str_vctr.resize(i + 1);
+		str_vctr[i].push_back(var);
+		str_vctr[i].push_back(str_thr.StartingTime.QuadPart);
+		str_vctr[i].push_back(str_thr.EndingTime.QuadPart);
+		++i;
+		//str_thr.cpu.stop();
+		//str_thr.ElapsedNanoseconds.QuadPart += str_thr.EndingTime.QuadPart - str_thr.StartingTime.QuadPart;
+		//str_thr.elapse += str_thr.cpu.elapsed().system + str_thr.cpu.elapsed().user;
+	}
+	if (varvar != 444) { varvar = 444; }
+	fstream file;
+	file.open("C:\\thr\\thread_" + to_string(str_thr.id) + ".txt", fstream::out | fstream::trunc);
+	for (size_t i = 0; i < str_vctr.size(); ++i) {
+		file << setw(4) << to_string(str_vctr[i][0]) << "    St.time " << to_string(str_vctr[i][1]) << "    En.time " << to_string(str_vctr[i][2]) << "    El.time " << to_string((str_vctr[i][2] - str_vctr[i][1])) << endl;
+	}
+	//str_thr.cpu.stop();
+	//QueryPerformanceCounter(&str_thr.EndingTime);
+	//str_thr.elapse = boost::chrono::nanoseconds(str_thr.cpu.elapsed().wall);
+	//str_thr.ElapsedNanoseconds.QuadPart += str_thr.EndingTime.QuadPart - str_thr.StartingTime.QuadPart;
+	//str_thr.ElapsedNanoseconds.QuadPart *= 1000000000 / Frequency.QuadPart;
+	//file << "Elapsed time: " << str_thr.ElapsedNanoseconds.QuadPart << endl;
+	//file << "Elapsed time: " << str_thr.elapse.count() << endl;
+	file.close();
+}
+
+class A {
+public:
+	A() {};
+	~A() {};
+	void f(int a) { cout << "AAAAAA" << a << endl; }
+};
+
+class B : public A {
+public:
+	B() {};
+	~B() {};
+	void f(int b) { cout << "BBBBBB" << b << endl; }
+};
+
+int main() {
+	EvenOddFunctor evenodd;
+	LARGE_INTEGER Frequency;
+	LARGE_INTEGER StartingTime, EndingTime, ElapsedNanoseconds;
+	int my_list[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	void thread_func(THR &, LARGE_INTEGER &);
+	int roll_die(boost::random::mt19937 &gen);
+
+	int a = 1, b = 2;
+	A *A1 = new A();
+	A *B1 = new B();
+	A1->f(a);
+	B1->f(b);
+
+	QueryPerformanceFrequency(&Frequency);
+	QueryPerformanceCounter(&StartingTime);
+	boost::thread ** thrd_arr = new boost::thread *[4];
+	boost::thread_group * rand_threads = new boost::thread_group;
+	THR * str_thr_arr = new THR[4];
+	for (int i = 0; i < 4; ++i) {
+		str_thr_arr[i].id = i + 1;
+		thrd_arr[i] = new boost::thread(boost::bind(&thread_func, boost::ref(str_thr_arr[i]), boost::ref(Frequency)));
+		rand_threads->add_thread(thrd_arr[i]);
+	}
+	rand_threads->join_all();
+	rand_threads->~thread_group();
+	QueryPerformanceCounter(&EndingTime);
+	ElapsedNanoseconds.QuadPart = (EndingTime.QuadPart - StartingTime.QuadPart) * 1000000000 / Frequency.QuadPart;
+	EvenOddFunctor() = for_each(my_list,
+		my_list + sizeof(my_list) / sizeof(my_list[0]),
+		EvenOddFunctor());
+
+	std::cout << "Чумма ч™тных: " << evenodd.even_sum() << "\n";
+	std::cout << "Чумма неч™тных: " << evenodd.odd_sum() << std::endl;
+
+	// вывод:
+	// Чумма ч™тных: 30
+	// Чумма неч™тных: 25
+}
